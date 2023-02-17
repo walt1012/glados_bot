954e9ded16f4fdd4edfd47447745949bb343e591
walt1012 <walt1012@foxmail.com>
2023-02-17 17:45:10 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -17,7 +17,7 @@ def start():
     referer = "https://glados.rocks/console/checkin"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
     payload = {
-        'token': 'glados_network'
+        'token': 'glados.network'
     }
     checkin = scraper.post(url,
                             headers={'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent,
