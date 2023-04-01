5d2bcdd2ab92db8cf0e8b074d965d7d9e27f2dbe
walt1012 <walt1012@foxmail.com>
2023-04-01 22:43:07 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -36,10 +36,8 @@ def start():
         time2 = state2.json()['data']['leftDays'].split('.')[0]
 
         url3 = 'https://api.day.app/VHBmnRjJh7fKC47ZcELLr/'
-            .__add__('🚩🚩🚩🚩打卡1🚩🚩🚩🚩')
-            .__add__(mess1).__add__('，').__add__(time1).__add__(' days left')
-            .__add__('🚩🚩🚩🚩打卡2🚩🚩🚩🚩')
-            .__add__(mess2).__add__('，').__add__(time2).__add__(' days left')
+            .__add__('🚩🚩🚩🚩打卡1🚩🚩🚩🚩').__add__(mess1).__add__('，').__add__(time1).__add__(' days left')
+            .__add__('🚩🚩🚩🚩打卡2🚩🚩🚩🚩').__add__(mess2).__add__('，').__add__(time2).__add__(' days left')
 
         scraper.get(url3)
     else:
