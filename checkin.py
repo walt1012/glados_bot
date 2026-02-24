c7cdeab9a6e19bb1e2e40bf3bcb52c336ad1adf7
shang wu <walt1012@foxmail.com>
2026-02-24 17:52:18 +0800

    Refactor check-in logic to use single cookie

--- checkin.py
+++ checkin.py
@@ -2,46 +2,26 @@ import requests, json, os
 import cloudscraper
 scraper = cloudscraper.create_scraper(disableCloudflareV1=True)
 
-# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-# sever = os.environ["SERVE"]
-# sever = "on"
-# 填写server酱sckey,不开启server酱则不用填
-# sckey = os.environ["SCKEY"]
-# 填入glados账号对应cookie
-# cookie1 = os.environ["COOKIE1"]
-cookie2 = os.environ["COOKIE2"]
+cookie = os.environ["COOKIE"]
 
 def start():
-    url = "https://glados.rocks/api/user/checkin"
-    url2 = "https://glados.rocks/api/user/status"
+    url1 = "https://glados.cloud/console/checkin"
+    url2 = "https://glados.cloud/console/account"
     origin = "https://glados.rocks"
     referer = "https://glados.rocks/console/checkin"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
     payload = {
         'token': 'glados.one'
     }
-    # checkin1 = scraper.post(url, headers={'cookie': cookie1, 'referer': referer, 'origin': origin, 'user-agent': useragent,
-    #                                  'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload))
-    checkin2 = scraper.post(url, headers={'cookie': cookie2, 'referer': referer, 'origin': origin, 'user-agent': useragent,
+    checkin = scraper.post(url1, headers={'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent,
                                  'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload))
-    
-    # state1 = scraper.get(url2, headers={'cookie': cookie1, 'referer': referer, 'origin': origin, 'user-agent': useragent})
-    state2 = scraper.get(url2, headers={'cookie': cookie2, 'referer': referer, 'origin': origin, 'user-agent': useragent})
+    state = scraper.get(url2, headers={'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent})
 
-    if ('message' in checkin2.text):
-        # mess1 = checkin1.json()['message']
-        mess2 = checkin2.json()['message']
-
-        # time1 = state1.json()['data']['leftDays']
-        time2 = state2.json()['data']['leftDays']
-
-        url3 = 'https://api.day.app/VHBmnRjJh7fKC47ZcELLr/🚩🚩🚩🚩打卡🚩🚩🚩🚩'.__add__(str(time2)).__add__(' days left')
-
-        scraper.get(url3)
-    else:
-        url4 = 'https://api.day.app/VHBmnRjJh7fKC47ZcELLr/COOKIE过期'
-        scraper.get(url4)
-
+    if ('message' in checkin.text):
+        mess = checkin.json()['message']
+        time = state.json()['data']['leftDays']
+        print(mess)
+        print(time)
 
 def main_handler(event, context):
     return start()
